12ab08769c3acc3b421e775fb51aa024798025de
Alexis Jones <21406612@bucks.ac.uk>
2019-01-26 11:47:15 +0000

    Models added

---
+++ HungryBirds/Models/Meals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HungryBirds.Models
+{
+    public class Meals
+    {
+        public int MealsId { get; set; }
+        public int Week { get; set; }
+        public String Dayofweek { get; set; }
+        public String MealName { get; set; }
+        public Boolean Chosenmeal { get; set; }
+        public string Colour { get; set; }
+        public int Cost { get; set; }
+
+    }
+}
