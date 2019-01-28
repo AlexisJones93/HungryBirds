c4a18c09768db104fa4545381e7ed1236e253bb4
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-01-28 09:42:25 +0000

    chnages to student.cs

--- HungryBirds/Models/Selection.cs
+++ HungryBirds/Models/Selection.cs
@@ -7,7 +7,11 @@ namespace HungryBirds.Models
 {
     public class Selection
     {
+        public DateTime Date { get; set; }
         public int SelectionId { get; set; }
-
+        public int MealId { get; set; }
+        public int StudentId { get; set; }
+        
     }
+
 }
